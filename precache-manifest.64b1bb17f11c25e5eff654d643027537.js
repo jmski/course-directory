ea8909704576476b7075d75656e5c613a58d3ac5
= <geluajm@gmail.com>
2021-03-01 13:01:09 -0500

    Updates

---
+++ precache-manifest.64b1bb17f11c25e5eff654d643027537.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "481bb8c14627a94a5d7c",
+    "url": "/course-directory/static/css/main.d66ebab9.chunk.css"
+  },
+  {
+    "revision": "481bb8c14627a94a5d7c",
+    "url": "/course-directory/static/js/main.481bb8c1.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "378d85b39b770e4d9f696d6f79c0a881",
+    "url": "/course-directory/index.html"
+  }
+];
